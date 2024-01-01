37e94846ee4d6a51dc56c47ae3f0b696708fcd0b
srivatsan-suresh <srivatsan.suresh@outlook.com>
2024-01-01 10:33:06 +0530

    Projects

---
+++ src/components/ProjectDetailsPage.js
@@ -0,0 +1,28 @@
+// ProjectDetailsPage.js
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import { useParams } from 'react-router-dom';
+
+const ProjectDetailsPage = ({ projects }) => {
+  const { id } = useParams();
+  const projectId = parseInt(id, 10);
+  const project = projects.find((p) => p.id === projectId);
+
+  if (!project) {
+    return <div>Project not found</div>;
+  }
+
+  return (
+    <div className="container">
+      <h2>{project.title}</h2>
+      {project.details && <ReactMarkdown>{project.details}</ReactMarkdown>}
+      <a href={project.githubRepo} className="btn teal-button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
+              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+            </svg> Repository URL
+      </a>
+    </div>
+  );
+};
+
+export default ProjectDetailsPage;
---
+++ src/components/ProjectsPage.js
@@ -0,0 +1,46 @@
+// ProjectsPage.js
+import React, { useState } from 'react';
+import ProjectCard from './ProjectCard';
+import ReactPaginate from 'react-paginate';
+import './ProjectsPage.css'; // Import your CSS for styling
+
+const ProjectsPage = ({ projects }) => {
+  const [currentPage, setCurrentPage] = useState(0);
+  const projectsPerPage = 5;
+
+  const handlePageClick = (data) => {
+    setCurrentPage(data.selected);
+  };
+
+  const indexOfLastProject = (currentPage + 1) * projectsPerPage;
+  const indexOfFirstProject = indexOfLastProject - projectsPerPage;
+  const currentProjects = projects.slice(indexOfFirstProject, indexOfLastProject);
+
+  return (
+    <div className="container">
+      <h2 style={{ color: 'rgb(117,20,124)' }}>Projects</h2>
+      {currentProjects.map((project) => (
+        <div className="row mb-3" key={project.id}>
+          <div className="col-md-12">
+            <ProjectCard project={project} />
+          </div>
+        </div>
+      ))}
+      <div className="pagination-container">
+        <ReactPaginate
+          pageCount={Math.ceil(projects.length / projectsPerPage)}
+          pageRangeDisplayed={5}
+          marginPagesDisplayed={2}
+          onPageChange={handlePageClick}
+          containerClassName={'pagination'}
+          activeClassName={'active'}
+          initialPage={currentPage}
+          previousLabel={'Previous'}
+          nextLabel={'Next'}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ProjectsPage;
