692c9a8e76a935cb0dc86d244ae007920ef69921
Ayush <99096397+ayush4345@users.noreply.github.com>
2024-02-22 22:15:43 +0530

    fixed import error

--- src/app/iet/page.js
+++ src/app/iet/page.js
@@ -1,5 +1,5 @@
 "use client"
-import {useState} from react;
+import {useState} from "react";
 import About from "@/components/about";
 import SIG from "@/components/sig";
 import Link from 'next/link'
