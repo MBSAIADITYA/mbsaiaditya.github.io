80d2b53e05289a55b351e7756ebd11fa6abee33e
Ayush <99096397+ayush4345@users.noreply.github.com>
2024-02-22 22:18:05 +0530

    removed bugs

--- src/app/iet/page.js
+++ src/app/iet/page.js
@@ -1,5 +1,5 @@
 "use client"
-import {useState} from "react";
+import { useState } from "react";
 import About from "@/components/about";
 import SIG from "@/components/sig";
 import Link from 'next/link'
@@ -8,18 +8,10 @@ import Hero from "@/components/hero";
 
 
 export default function Home() {
-    const [data, setData] = useState([]);
-    const fetch = async () => {
-        const response = await fetch("https://tetragram.codered.cloud/api/v2/pages/?type=events.EventsPage&fields=*");
-        const result = await response.json()
-        console.log(result.items)
-        setData(result.items)
-    }
-    fetch()
     return (
         <>
-             <Link aria-current="page" href={"/blogs"} passHref></Link>
-             <Link aria-current="team" href={"/team"} passHref></Link>
+            <Link aria-current="page" href={"/blogs"} passHref></Link>
+            <Link aria-current="team" href={"/team"} passHref></Link>
             <Hero />
             <About />
             <SIG />
