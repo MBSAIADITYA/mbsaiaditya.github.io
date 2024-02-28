d0fd570fcc445684371b2f78516188cd90e61b45
Ayush <99096397+ayush4345@users.noreply.github.com>
2024-02-29 00:29:35 +0530

    change hero text

--- src/components/hero.js
+++ src/components/hero.js
@@ -8,18 +8,18 @@ export default function Hero() {
                 <Particles />
                 <div className="flex items-center sm:justify-between w-full px-10 flex-col-reverse md:flex-row z-10">
                     <div className="flex flex-col flex-auto w-full md:w-1/2 justify-center">
-                        <h1 className="text-black text-4xl md:text-5xl font-bold">Your potential.<br /> Our passion.<br />
+                        <h1 className="text-black text-4xl md:text-5xl font-bold">Igniting Innovation
                         </h1>
                         <h1 className=' text-[#631c6f] text-4xl md:text-5xl font-bold mb-4'>
                             <Typewriter
                                 options={{
-                                    strings: ["Engineering to inspire.", "Exploring innovation.", "Designing to ideate.", "Digitally serving."],
+                                    strings: ["Empowering Futures.", "Inspiring Tomorrow's Engineers.", "Designing to ideate.", "Unlocking potential."],
                                     autoStart: true,
                                     loop: true,
                                 }}
                             />
                         </h1>
-                        <h2 className=" text-2xl text-[#484848] mb-12 ">We are a team of motivated students pledged to promote technological growth accross the globe.
+                        <h2 className=" text-2xl text-[#484848] mb-12 ">Empowering future innovators to promote global engineering and innovations.
                         </h2>
                     </div>
                     <div className="hidden md:flex w-1/2 justify-center">
